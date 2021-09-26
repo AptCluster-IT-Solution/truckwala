8ee76cf52933cc0fa65b1771488634370be7a4f6
Mahesh <mahyess@users.noreply.github.com>
2021-09-26 23:04:08 +0545

    build.sh changed

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,7 +6,7 @@ chmod 777 ./*
 
 # shellcheck disable=SC2087
 ssh -o StrictHostKeyChecking=no $SSH_USER@$EC2_PUBLIC_IP_ADDRESS 'bash -s' <<ENDSSH
-  cd /home/$SSH_USER/greenbazar
+  cd /home/$SSH_USER/truckbooking
   git pull
   aws ecr get-login-password --region $EC2_REGION | docker login --username $ECR_USER --password-stdin $ECR_WEB_IMAGE
   docker-compose pull
