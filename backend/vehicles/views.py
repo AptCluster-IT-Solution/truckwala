1593699d978ceb949e111999c84bec78d4073906
Mahesh <mahyess@users.noreply.github.com>
2021-09-29 02:43:29 +0545

    vehicle api permission mgmt

--- backend/vehicles/views.py
+++ backend/vehicles/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from main.custom.permissions import ActionBasedPermission, IsPosterOrReadOnly, IsDriver
 from vehicles.models import Vehicle, VehicleCategory
 from vehicles.serializers import VehicleSerializer, VehicleCategorySerializer
 
@@ -8,6 +9,11 @@ from vehicles.serializers import VehicleSerializer, VehicleCategorySerializer
 class VehicleModelViewSet(viewsets.ModelViewSet):
     serializer_class = VehicleSerializer
     queryset = Vehicle.objects.all()
+    permission_classes = [ActionBasedPermission]
+    action_permissions = {
+        IsPosterOrReadOnly: ["update", "partial_update", "destroy", "list", "retrieve"],
+        IsDriver: ["create"],
+    }
 
     def perform_create(self, serializer):
         serializer.save(driver=self.request.user.driver_profile)
