53fe1437af62c79d0e6947258bfea753479870e5
Mahesh <mahyess@users.noreply.github.com>
2021-12-01 18:21:22 +0545

    clear unused containers in build workflow

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -11,4 +11,5 @@ ssh -o StrictHostKeyChecking=no $SSH_USER@$EC2_PUBLIC_IP_ADDRESS 'bash -s' <<END
   aws ecr get-login-password --region $EC2_REGION | docker login --username $ECR_USER --password-stdin $ECR_WEB_IMAGE
   docker-compose pull
   docker-compose -f docker-compose.prod.yml up -d --build
+  docker volume prune
 ENDSSH
