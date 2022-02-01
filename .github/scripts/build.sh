f9b12f4f71c4b883cd6ccd4bebfb1c71709627f7
Mahesh <mahyess@users.noreply.github.com>
2022-02-01 18:50:20 +0545

    fix: docker build on necessary changes only

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -10,6 +10,7 @@ ssh -o StrictHostKeyChecking=no $SSH_USER@$EC2_PUBLIC_IP_ADDRESS 'bash -s' <<END
   git pull
   aws ecr get-login-password --region $EC2_REGION | docker login --username $ECR_USER --password-stdin $ECR_WEB_IMAGE
   docker-compose pull
+  docker-compose -f docker-compose.prod.yml down
   docker-compose -f docker-compose.prod.yml up -d --build
   docker system prune -f
 ENDSSH
